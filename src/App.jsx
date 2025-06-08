ed9efa5f10300ab58ab509eae4eb12b74096cab5
Jeff Wang <wa.jeff@outlook.com>
2025-06-08 19:13:11 -0400

    Created CardPage component that takes in props to display corresponding scraped credit card promotions

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CardPage from './components/CardPage/CardPage'
 import Home from './components/Home/Home'
+
+
 function App() {
-  const AmexCobalt = 'amexcobalt'
+  const amexCobalt = import.meta.env.VITE_AMEX_COBALT_ROBOT_ID
+
   return (
      <Router>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path="/amex" element={<CardPage card={AmexCobalt} />} />
+        <Route path="/amex" element={<CardPage card={amexCobalt} />} />
       </Routes>
     </Router>
   )
--- src/components/CardPage/CardPage.jsx
+++ src/components/CardPage/CardPage.jsx
@@ -1,11 +1,41 @@
-const CardPage = ({card}) => {
-  
-  return (
-    <div>
-      
-      
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
-    </div>
+const CardPage = ({ card }) => {
+  const [promotion, setPromotion] = useState('Loading...')
+  useEffect(() => {
+    if (!card){
+      return 'Error'
+    }
+
+    const fetchPromotion = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.browse.ai/v2/robots/${card}/tasks`,
+          {
+            headers: {
+              Authorization: `Bearer ${import.meta.env.VITE_ROBOT_API_KEY}`
+            }
+          }
+        )
+        const promo =
+          response.data?.result?.robotTasks?.items?.[0]?.capturedTexts?.Promotion ||
+          'No promotion found'
+        setPromotion(promo)
+      } catch (error) {
+        console.error('Error fetching tasks:', error)
+        setPromotion('Error fetching promotion')
+      }
+    }
+
+    fetchPromotion()
+  }, [card])
+
+  return (
+    <>
+      <h2>Promotion:</h2>
+      <p>{promotion}</p>
+    </>
   )
 }
 
