d2e8f8196bcdac441c2270255637132375114e88
Jeff Wang <wa.jeff@outlook.com>
2025-06-08 22:45:06 -0400

    Implented Gemini API to output structured response to scraped amex cobalt offer data

--- src/components/CardPage/CardPage.jsx
+++ src/components/CardPage/CardPage.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import { GoogleGenAI } from "@google/genai";
+
 
 const CardPage = ({ card }) => {
-  const [promotion, setPromotion] = useState('Loading...')
+  const [promotion, setPromotion] = useState('')
   useEffect(() => {
     if (!card){
       return 'Error'
@@ -19,7 +21,7 @@ const CardPage = ({ card }) => {
           }
         )
         const promo =
-          response.data?.result?.robotTasks?.items?.[0]?.capturedTexts?.Promotion ||
+          response.data?.result?.robotTasks?.items?.[2]?.capturedTexts?.Promotion ||
           'No promotion found'
         setPromotion(promo)
       } catch (error) {
@@ -27,10 +29,54 @@ const CardPage = ({ card }) => {
         setPromotion('Error fetching promotion')
       }
     }
-
+    
     fetchPromotion()
   }, [card])
 
+
+useEffect(()=>{
+  if (
+    !promotion ||
+    promotion === 'No promotion found' ||
+    promotion === 'Error fetching promotion'
+  ) return;
+  const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
+ 
+  async function main() {
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents:
+        `${promotion} please fill out the membership fee to its exact value`, 
+      config: {
+        responseMimeType: "application/json",
+        responseSchema: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            totalPoints: {
+              type: "integer",
+            },
+            totalSpendRequired: {
+              type: "integer",
+            },
+            totalMembershipFee:{
+              type: "integer",
+            },
+          },   
+        },
+        },
+      propertyOrdering: ["totalPoints", "totalSpendRequired","totalMembershipFee"],
+      },
+    });
+  console.log(response.text);
+  }
+  main()
+  },[promotion]
+)
+  
+  
   return (
     <>
       <h2>Promotion:</h2>
