1b95e09beb6e7bdfe8032358978f9182a5501d94
Jeffery Wang <wa.jeff@outlook.com>
2026-03-12 11:28:57 -0400

    feature: card stack feature: barebones searchbar other styling choices

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,21 +1,37 @@
-import styles from './Home.module.css'
-import { Link } from 'react-router-dom'
-
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  
   return (
     <div>
-      <h1 className={styles.title}>Card Chart</h1>
-      <ul className={styles.linkContainer}>
-        <Link to='/amex_cobalt'> Amex Cobalt</Link>
-        <Link to='/amex_platinum'> Amex Platinum</Link>
-        <Link to='/amex_gold'> Amex Gold</Link>
-      </ul>
-      
+      <div className={styles.hero}>
+        <h1 className={styles.title}>Card Chart</h1>
+        <input id="searchBar" className={styles.search}></input>
 
+        <section className={styles.cardsContainer}>
+          <ul className={styles.linkContainer}>
+            <Link to="/amex_cobalt" aria-label="Amex Cobalt" className={styles.cardLink}>
+              <img
+                src="/explorer_2019_ca_di_dod_480x304.avif"
+                alt="Amex Cobalt"
+                className={styles.card}
+              />
+            </Link>
+
+            <Link to="/amex_platinum" className={styles.cardLink}>
+              <img
+                src="/explorer_2019_ca_di_dod_480x304.avif"
+                alt="Amex Cobalt"
+                className={styles.card}
+              />
+            </Link>
+
+            <Link to="/amex_gold"> Amex Gold</Link>
+          </ul>
+        </section>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
